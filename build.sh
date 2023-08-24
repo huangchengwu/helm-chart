1d1a584d49d04fb0c3f6b5e7ee0b1d7c71eacff8
huangchengwu <751164212@qq.com>
2023-08-24 14:33:24 +0800

    add

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf chart/*.tgz
+rm -rf chart/index.yaml
+rm -rf *.tgz
+
+for k in $(ls -l | grep -v -E "chart|make|README.md" | awk '{print \$9}'); do
+    helm package $k
+done
+mv *.tgz chart
+helm repo index chart --url https://huangchengwu.github.io/helm-chart/chart
+git add *
+git commit -m "add"
+git push -f
